0a7b82c09473d2dd6e441762b3fa923c33e89ea4
Samuel Cornet <samuel@noukod.com>
2019-01-30 21:30:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ function distanceFromHqInBlocks(block){
  }
 
  function distanceTravelledInFeet (pointBlockA, pointBlockB){
-     return pointBlockA > pointBlockB ?
-    (pointBlockA - pointBlockB) * 264 : (pointBlockB - pointBlockA) * 264
+     return pointBlockA > pointBlockB ? (pointBlockA - pointBlockB) * 264 : (pointBlockB - pointBlockA) * 264
+  }
+  function calculatesFarePrice (startBlock, endBlock){
+      const numFeet = distanceTravelledInFeet(startBlock, endBlock)
+      if (numFeet <= 400) {
+          return 0;
+        } else if (numFeet > 400 && numFeet <= 2000) {
+          return 0.02 * (numFeet - 400);
+        } else if (numFeet > 2000 && numFeet < 2500) {
+          return 25;
+        } else {
+          return 'cannot travel that far';
+      }
   }
